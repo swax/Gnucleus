de1d37628fd9146a7746424e1e8bdd8197f79404
swabby <swabby>
2005-03-22 18:57:52 +0000

    GPL License update for 2005

--- NetSetupNew.cpp
+++ NetSetupNew.cpp
@@ -1,27 +1,29 @@
 /********************************************************************************
 
 	Gnucleus - An Application for the Gnutella Network
-    Copyright (c) 2000-2004 John Marshall
+    Copyright (C) 2000-2005 John Marshall Group
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License.
+    This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	For support, questions, comments, etc...
-	E-Mail:
-		swabby@c0re.net
+	By contributing code you grant John Marshall Group an unlimited, non-exclusive
+	license your contribution.
+
+	For support, questions, commercial use, etc...
+	E-Mail: swabby@c0re.net
 
 ********************************************************************************/
-
 
 #include "stdafx.h"
 #include "gnucleus.h"
